6199cdea1475b3e06891efb877a41ab64e8a2937
Mansur <example@gmail.com>
2024-10-16 18:15:37 +0500

    first commit

---
+++ inde.js
@@ -0,0 +1,20 @@
+
+// const favouriteColur = [];
+
+// favouriteColur[0] = prompt("What's your favourite color #1,")
+// favouriteColur[1] = prompt("What's your favourite color #2,")
+// favouriteColur[2] = prompt("What's your favourite color #3,")
+
+
+// let incr = 10
+// let decr = 10
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
